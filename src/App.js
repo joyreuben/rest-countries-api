3d217df042344eba4212d05ac311c5246118fa57
joyreuben <okebugwujoy@gmail.com>
2022-10-27 00:46:57 +0100

    Change name

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Countries from "./components/Countries";
+import Header from "./components/Header"
+import Filter from './components/Filter'
+import Country from "./components/Country";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Header/>
+        <Route exact path="/">
+          <Filter />
+          <Countries />
+        </Route>
+        <Route path="/countries/:name"  children={<Country />}>
+        </Route>
+      </>  
+    </Router>
   );
 }
 
